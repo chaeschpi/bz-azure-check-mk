45cb4dc019b0809310dfc33b300d684d54ee1c96
Daniel Kasper <daniel.kasper@umb.ch>
2021-06-18 13:22:54 +0200

    code some logic

--- special/agent_bz-azure-special.py
+++ special/agent_bz-azure-special.py
@@ -4,16 +4,61 @@ import json
 ######## variables declaration ###################
 args= "null"
 r = "null" 
-baseurl = 'https://bzmonitorapi-prd.azurewebsites.net/api/Function1'
+baseurl = 'https://bzmonitorapi-prd.azurewebsites.net/api/getStatus'
 Query = "null"
 ####### function declaration ####################
 
 def process_json_response(text):
     _json = json.loads(text)
-
     for k ,v  in _json.items():
-        print (f"<<<{k}>>>")
-        print(v)
+        if (k == 'advisorRecommendationsSummary'): 
+            advisorRecommendationsSummary(v)
+        elif k == 'backupJobsSummary':
+                backupjob(v)
+        elif k == 'resourceHealthSummary': 
+            healthSummary(v)
+        elif (k == 'serviceHealthSummary'):
+                serviceHealthSummary(v)
+
+
+
+def advisorRecommendationsSummary(json): 
+    summary = json.get('advisorSummary')
+    print ('<<<az-bz_Advisor>>>')
+    for k,v in summary.items():
+        if k == 'totalItems':
+            continue
+        printstr = ''
+        for e in v.values():
+            printstr = printstr + str(e) + ' '
+        print(k, printstr)
+# advisor (high, mid, low, suppression)    
+
+def backupjob(json): 
+    summary = json.get('backupSummary')
+    print ('<<<az-bz_Backup>>>')
+    for k,v in summary.items():
+        print('backup'+k,v)
+    return 
+
+def healthSummary(json): 
+    summary = json.get('healthSummary')
+    print ('<<<az-bz_Health>>>')
+    for k,v in summary.items():
+        print(k,v)
+
+def serviceHealthSummary(json): 
+    summary = json.get('serviceSummary')
+    print ('<<<az-bz_Service>>>')
+    for k,v in summary.items():
+        print(k,v)
+    
+
+
+
+
+
+
 
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser()
@@ -31,6 +76,4 @@ if r.status_code == 200:
    process_json_response(r.text)
 else: 
     print("Request Error "+r.reason)
-
-
 
