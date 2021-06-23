19d4ed3cd9869f52ef1c4023e4783f2505a7fe88
Daniel Kasper <daniel.kasper@umb.ch>
2021-06-23 13:10:19 +0200

    remove extension

--- devilery.sh
+++ devilery.sh
@@ -7,9 +7,9 @@ cp /root/bz-azure-check-mk/wato/bz-azurespecial_register.py /monitoring/check_mk
 
 ## Special Agent  
 
-cp /root/bz-azure-check-mk/special/agent_bz-azure-special.py /monitoring/check_mk/sites/umb_bluezone/local/share/check_mk/agents/special
+cp /root/bz-azure-check-mk/special/agent_bz-azure-special /monitoring/check_mk/sites/umb_bluezone/local/share/check_mk/agents/special
 chmod +x /monitoring/check_mk/sites/umb_bluezone/local/share/check_mk/agents/special/*
 ## copy Checks 
 
-cp /root/bz-azure-check-mk/checks/agent_bz-azure-special.py /monitoring/check_mk/sites/umb_bluezone/local/share/check_mk/checks
+cp /root/bz-azure-check-mk/checks/agent_bz-azure-special /monitoring/check_mk/sites/umb_bluezone/local/share/check_mk/checks
 chmod +x /monitoring/check_mk/sites/umb_bluezone/local/share/check_mk/checks/agent_bz-azure-special.py
