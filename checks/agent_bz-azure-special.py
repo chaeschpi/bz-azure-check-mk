6dd55eaed284e41300c95aa22bf45e8a702b7c42
Daniel Kasper <daniel.kasper@umb.ch>
2021-06-23 10:57:48 +0200

    mod

--- checks/agent_bz-azure-special.py
+++ checks/agent_bz-azure-special.py
@@ -8,4 +8,4 @@ def agent_myspecal_arguments(params):
 
     return args
 
-special_agent_info['myspecial'] = agent_myspecal_arguments
+special_agent_info['bz-azure-special'] = agent_myspecal_arguments
