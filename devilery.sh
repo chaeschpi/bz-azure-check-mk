ba18b53f362011c24b4d6be145c5e05220e20340
Daniel Kasper <daniel.kasper@umb.ch>
2021-06-23 12:07:05 +0200

    mod

--- devilery.sh
+++ devilery.sh
@@ -4,7 +4,12 @@
 mk_path="/monitoring/check_mk/sites/umb_bluezone/"
 
 cp /root/bz-azure-check-mk/wato/bz-azurespecial_register.py /monitoring/check_mk/sites/umb_bluezone/local/share/check_mk/web/plugins/wato
-
+chmod +x 
 ## Special Agent  
 
 cp /root/bz-azure-check-mk/special/agent_bz-azure-special.py /monitoring/check_mk/sites/umb_bluezone/local/share/check_mk/agents/special
+chmod +x /monitoring/check_mk/sites/umb_bluezone/local/share/check_mk/agents/special/*
+## copy Checks 
+
+cp /root/bz-azure-check-mk/checks/agent_bz-azure-special.py /monitoring/check_mk/sites/umb_bluezone/local/share/check_mk/checks
+chmod +x /monitoring/check_mk/sites/umb_bluezone/local/share/check_mk/checks/agent_bz-azure-special.py
